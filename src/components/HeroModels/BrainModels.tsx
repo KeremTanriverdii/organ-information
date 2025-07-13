4947c83ed663625886423dd55b685cff7810c1da
Ahmet Kerem <ahmetk.tanriverdi@gmail.com>
2025-07-13 19:13:11 +0300

    Added shadcn components, sidebar and theme changes.

--- src/components/HeroModels/BrainModels.tsx
+++ src/components/HeroModels/BrainModels.tsx
@@ -3,6 +3,8 @@ import { Environment, OrbitControls } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 import { useState } from "react";
 import '../../App.css'
+import { Card, CardAction, CardDescription, CardHeader } from "../ui/card";
+import { Button } from "../ui/button";
 
 export default function BrainModels() {
     const [isSelected, setIsSelected] = useState<string | null>(null);
@@ -11,124 +13,168 @@ export default function BrainModels() {
         switch (key) {
             case 'materials2':
                 return (
-                    <>
-                        <h2>{isSelected}</h2>
+                    <Card>
+                        <CardHeader>{isSelected}</CardHeader>
+                        <CardDescription>is detailed for brain of parts</CardDescription>
                         <ul>
                             <li>lorem1</li>
                             <li>Lorem, ipsum.</li>
                             <li>Lorem, ipsum dolor.</li>
                         </ul>
-                    </>
+                        <CardAction>
+                            <Button onClick={() => setIsSelected(null)}>Back</Button>
+                        </CardAction>
+                    </Card>
                 )
             case 'materials2_1':
                 return (
-                    <>
-                        <h2>{isSelected}Temporal Lobe</h2>
+                    <Card>
+                        <CardHeader>{isSelected}</CardHeader>
+                        <CardDescription>is detailed for brain of parts</CardDescription>
                         <ul>
                             <li>lorem1</li>
                             <li>Lorem, ipsum.</li>
                             <li>Lorem, ipsum dolor.</li>
                         </ul>
-                    </>
+                        <CardAction>
+                            <Button onClick={() => setIsSelected(null)}>Back</Button>
+                        </CardAction>
+                    </Card>
                 )
             case 'materials2_2':
                 return (
-                    <>
-                        <h2>{isSelected} Pareital lobe</h2>
+                    <Card>
+                        <CardHeader>{isSelected}</CardHeader>
+                        <CardDescription>is detailed for brain of parts</CardDescription>
                         <ul>
                             <li>lorem1</li>
                             <li>Lorem, ipsum.</li>
                             <li>Lorem, ipsum dolor.</li>
                         </ul>
-                    </>
+                        <CardAction>
+                            <Button onClick={() => setIsSelected(null)}>Back</Button>
+                        </CardAction>
+                    </Card>
                 )
             case 'materials2_3':
                 return (
-                    <>
-                        <h2>{isSelected}</h2>
+                    <Card>
+                        <CardHeader>{isSelected}</CardHeader>
+                        <CardDescription>is detailed for brain of parts</CardDescription>
                         <ul>
                             <li>lorem1</li>
                             <li>Lorem, ipsum.</li>
                             <li>Lorem, ipsum dolor.</li>
                         </ul>
-                    </>
+                        <CardAction>
+                            <Button onClick={() => setIsSelected(null)}>Back</Button>
+                        </CardAction>
+                    </Card>
                 )
             case 'materials2_4':
                 return (
-                    <>
-                        <h2>{isSelected}Pillutary gland</h2>
+                    <Card>
+                        <CardHeader>{isSelected}</CardHeader>
+                        <CardDescription>is detailed for brain of parts</CardDescription>
                         <ul>
                             <li>lorem1</li>
                             <li>Lorem, ipsum.</li>
                             <li>Lorem, ipsum dolor.</li>
                         </ul>
-                    </>
+                        <CardAction>
+                            <Button onClick={() => setIsSelected(null)}>Back</Button>
+                        </CardAction>
+                    </Card>
                 )
             case 'materials2_5':
                 return (
-                    <>
-                        <h2>{isSelected} Occopital Lobe</h2>
+                    <Card>
+                        <CardHeader>{isSelected}</CardHeader>
+                        <CardDescription>is detailed for brain of parts</CardDescription>
                         <ul>
                             <li>lorem1</li>
                             <li>Lorem, ipsum.</li>
                             <li>Lorem, ipsum dolor.</li>
                         </ul>
-                    </>
+                        <CardAction>
+                            <Button onClick={() => setIsSelected(null)}>Back</Button>
+                        </CardAction>
+                    </Card>
                 )
             case 'materials2_6':
                 return (
-                    <>
-                        <h2>{isSelected} Frontal Lobe</h2>
+                    <Card>
+                        <CardHeader>{isSelected}</CardHeader>
+                        <CardDescription>is detailed for brain of parts</CardDescription>
                         <ul>
                             <li>lorem1</li>
                             <li>Lorem, ipsum.</li>
                             <li>Lorem, ipsum dolor.</li>
                         </ul>
-                    </>
+                        <CardAction>
+                            <Button onClick={() => setIsSelected(null)}>Back</Button>
+                        </CardAction>
+                    </Card>
                 )
             case 'materials2_7':
                 return (
-                    <>
-                        <h2>{isSelected}</h2>
+                    <Card>
+                        <CardHeader>{isSelected}</CardHeader>
+                        <CardDescription>is detailed for brain of parts</CardDescription>
                         <ul>
                             <li>lorem1</li>
                             <li>Lorem, ipsum.</li>
                             <li>Lorem, ipsum dolor.</li>
                         </ul>
-                    </>
+                        <CardAction>
+                            <Button onClick={() => setIsSelected(null)}>Back</Button>
+                        </CardAction>
+                    </Card>
                 )
             case 'materials2_8':
                 return (
-                    <>
-                        <h2>{isSelected}</h2>
+                    <Card>
+                        <CardHeader>{isSelected}</CardHeader>
+                        <CardDescription>is detailed for brain of parts</CardDescription>
                         <ul>
                             <li>lorem1</li>
                             <li>Lorem, ipsum.</li>
                             <li>Lorem, ipsum dolor.</li>
                         </ul>
-                    </>
+                        <CardAction>
+                            <Button onClick={() => setIsSelected(null)}>Back</Button>
+                        </CardAction>
+                    </Card>
                 )
             case 'materials2_9':
                 return (
-                    <>
-                        <h2>{isSelected} Cerebellum</h2>
+                    <Card>
+                        <CardHeader>{isSelected}</CardHeader>
+                        <CardDescription>is detailed for brain of parts</CardDescription>
                         <ul>
                             <li>lorem1</li>
                             <li>Lorem, ipsum.</li>
                             <li>Lorem, ipsum dolor.</li>
                         </ul>
-                    </>
+                        <CardAction>
+                            <Button onClick={() => setIsSelected(null)}>Back</Button>
+                        </CardAction>
+                    </Card>
                 )
             case 'materials2_10':
                 return (
-                    <>
-                        <h2>{isSelected} Spinal Cord</h2>
+                    <Card>
+                        <CardHeader>{isSelected}</CardHeader>
+                        <CardDescription>is detailed for brain of parts</CardDescription>
                         <ul>
                             <li>lorem1</li>
                             <li>Lorem, ipsum.</li>
                             <li>Lorem, ipsum dolor.</li>
                         </ul>
-                    </>
+                        <CardAction>
+                            <Button onClick={() => setIsSelected(null)}>Back</Button>
+                        </CardAction>
+                    </Card>
                 )
             default:
                 return <p>Bu bölge için hakkında bilgi yok</p>
@@ -141,7 +187,6 @@ export default function BrainModels() {
                 {isSelected && (
                     <div>
                         {getInfo(isSelected)}
-                        <button onClick={() => setIsSelected(null)}>Back</button>
                     </div>
                 )}
             </div>
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,20 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { SidebarProvider, SidebarTrigger } from './components/ui/sidebar.tsx'
+import AppSidebar from './components/AppSidebar.tsx'
+import { ThemeProvider } from './components/Context/theme-provider.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ThemeProvider defaultTheme='dark' storageKey='vite-ui-theme'>
+      <SidebarProvider>
+        <AppSidebar />
+        <main>
+          <SidebarTrigger />
+          <App />
+        </main>
+      </SidebarProvider>
+    </ThemeProvider>
   </StrictMode>,
 )
